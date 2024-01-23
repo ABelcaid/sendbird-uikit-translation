347dae4c5e47d9ccf8d9da09e437ac3efb58db73
Abderrahim Belcaid <abderrahim.belcaid@ax-lab.com>
2024-01-23 13:45:07 +0100

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+import SendbirdProvider from '@sendbird/uikit-react/SendbirdProvider'
+import { RouterProvider } from 'react-router-dom'
+import { router as mobileRouter } from './MobileLayout/Router'
+import { router as desktopRouter } from './DesktopLayout/Router'
+
+function App() {
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
+  return (
+    <div className="sendbird-app">
+      <SendbirdProvider
+        appId="YOUR_APP_ID"
+        userId='YOUR_USER_ID'
+        nickname='YOUR_NICKNAME'
+        breakpoint={isMobile}
+      >
+        <RouterProvider router={isMobile ? mobileRouter : desktopRouter} />
+      </SendbirdProvider>
+    </div>
+  )
+}
+
+export default App
